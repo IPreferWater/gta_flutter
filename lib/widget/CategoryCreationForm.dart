16c95142b8b6b8b79acf18db7428219f1153d88e
IPreferWater <ipreferwater2@gmail.com>
2020-02-04 17:52:36 +0100

    prepare front for the bloc category

---
+++ lib/widget/CategoryCreationForm.dart
@@ -0,0 +1,126 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:gta_flutter/bloc/category_bloc/bloc.dart';
+import 'package:gta_flutter/model/category.dart';
+
+class CategoryFormDialog extends StatefulWidget{
+
+  final Category categoryToUpdate;
+
+  CategoryFormDialog({
+    this.categoryToUpdate
+  });
+
+  _CategoryFormDialogState createState() => _CategoryFormDialogState();
+
+}
+class _CategoryFormDialogState extends State<CategoryFormDialog> {
+
+  CategoryBloc _categoryBloc;
+
+  final _formKey = GlobalKey<FormState>();
+  final label = TextEditingController();
+
+  int categoryId;
+  List<String> parameters;
+
+  @override
+  void initState(){
+    super.initState();
+
+    _categoryBloc = BlocProvider.of<CategoryBloc>(context);
+    _categoryBloc.dispatch(LoadCategoriesEvent());
+
+
+    if(this.widget.categoryToUpdate!=null){
+      final Category creationToUpdate = widget.categoryToUpdate;
+      categoryId = creationToUpdate.id;
+
+      label.text = creationToUpdate.label;
+    }
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return
+      Dialog(
+        elevation: 0.0,
+        child: dialogContent(context),
+      );
+  }
+
+
+  dialogContent(BuildContext context) {
+    return Form(
+        key: _formKey,
+        child: ListView(
+            padding: const EdgeInsets.all(8),
+            children: <Widget>[
+              TextFormField(
+                controller: label,
+                decoration: const InputDecoration(
+                  icon: Icon(Icons.person),
+                  hintText: 'Title of the before item',
+                  labelText: 'Title',
+                ),
+                validator: (String value) {
+                  return value.isEmpty ? 'must not be empty' : null;
+                },
+              ),
+        ListView.builder(
+          itemCount: parameters.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text('${parameters[index]}'),
+            );
+          },
+        ),
+             /* Column(
+                children: <Widget>[
+                  RaisedButton(
+                    onPressed: getImageFromCamera,
+                    child: Text('camera'),
+                  ),
+                  RaisedButton(
+                    onPressed: () {
+                      getImageFromGallery(AFTER);
+                    },
+                    child: Text('gallery'),
+                  ),
+                  TextField(
+                    controller: afterImageUrl,
+                  )
+                ],
+              ),*/
+
+
+              Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 16.0),
+                  child: RaisedButton(
+                    onPressed: () {
+                      if (_formKey.currentState.validate()) {
+                        final categoryValided = Category(
+                          label: label.text
+                        );
+
+                        //TODO: make this code correct
+                        if(widget.categoryToUpdate!=null){
+                          categoryValided.id = widget.categoryToUpdate.id;
+                          _categoryBloc.dispatch(UpdateCategoryEvent(categoryValided));
+                        }else{
+                          _categoryBloc.dispatch(UpdateCategoryEvent(categoryValided));
+                        }
+
+                        //in this form we put the state to load only free qrCode, we want to retrieve the other
+                        _categoryBloc.dispatch(LoadCategoriesEvent());
+                        Navigator.of(context).pop();
+                      }
+                    },
+                    child: Text('Submit'),
+                  )),
+            ]
+        )
+    );
+  }
+}
