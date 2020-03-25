575646e89d9083bf4afe1317d5769e07fe5e86e3
IPreferWater <ipreferwater2@gmail.com>
2020-03-25 11:20:30 +0100

    clean code of ItemScreen

--- lib/screen/item_screen.dart
+++ lib/screen/item_screen.dart
@@ -1,23 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:gta_flutter/bloc/sub_category_bloc/bloc.dart';
+import 'package:gta_flutter/bloc/item_bloc/bloc.dart';
 import 'package:gta_flutter/model/category.dart';
 import 'package:gta_flutter/model/sub_category.dart';
 import 'package:gta_flutter/widget/add_button.dart';
 import 'package:gta_flutter/widget/carousel_slider.dart';
 import 'package:gta_flutter/widget/item_creation_form.dart';
-import 'package:gta_flutter/widget/sub_category_creation_form.dart';
 
 class ItemScreen extends StatefulWidget{
 
   final Category category;
   final SubCategory subCategory;
-  final SubCategoryBloc subCategoryBloc;
 
   ItemScreen({
     @required this.category,
-    @required this.subCategory,
-    @required this.subCategoryBloc
+    @required this.subCategory
   });
 
   _ItemScreenState createState() => _ItemScreenState();
@@ -25,14 +22,14 @@ class ItemScreen extends StatefulWidget{
 }
 class _ItemScreenState extends State<ItemScreen> {
 
-
-
-
-
+  ItemBloc _itemBloc;
+  
   @override
   void initState(){
     super.initState();
-
+    _itemBloc = BlocProvider.of<ItemBloc>(context);
+    _itemBloc.subCategory = widget.subCategory;
+    _itemBloc.add(LoadItemsEvent());
   }
 
   @override
@@ -50,14 +47,13 @@ class _ItemScreenState extends State<ItemScreen> {
 
   Widget _creationMenu() {
     return BlocBuilder(
-      bloc: widget.subCategoryBloc,
-      builder: (BuildContext context, SubCategoryState state){
-        if (state is SubCategoryLoadingState){
+      bloc: _itemBloc,
+      builder: (BuildContext context, ItemState state){
+        if (state is ItemLoadingState){
           return Center(
             child: CircularProgressIndicator(),
           );
-        } else if (state is SubCategoriesLoadingSuccessState){
-          print(widget.subCategory);
+        } else if (state is ItemsLoadingSuccessState){
           return
             Column(
                 children: <Widget>[
@@ -77,25 +73,6 @@ class _ItemScreenState extends State<ItemScreen> {
                     viewportFraction: 0.9,
                     aspectRatio: 2.0,
                   ),
-
-                  /*ListView.builder(
-                    shrinkWrap: true,
-                    itemCount: widget.subCategory.items.length,
-                    itemBuilder: (context, index) {
-                      final displayedItem = widget.subCategory.items[index];
-                      return ListTile(
-                        title: Text(displayedItem.label),
-                        subtitle: Text(
-                            'label : ${displayedItem.label} parameters : ${displayedItem.parameters}'),
-                        //trailing: _buildUpdateDeleteSubCategory(displayedSubCategory),
-                        onTap: () {
-                         /* Navigator.push(context,
-                              MaterialPageRoute(builder: (context) => SubCategoryScreen())
-                          );*/
-                        },
-                      );
-                    },
-                  ),*/
                   AddButton(
                       formDialog: ItemFormDialog(category: widget.category, subCategory: widget.subCategory,)
                   ),
@@ -111,30 +88,4 @@ class _ItemScreenState extends State<ItemScreen> {
       },
     );
   }
-
-  /*Row _buildUpdateDeleteSubCategory(SubCategory subCategory){
-    return Row(
-      mainAxisSize: MainAxisSize.min,
-      children: <Widget>[
-        IconButton(
-          icon: Icon(Icons.edit),
-          onPressed: () {
-            showDialog(
-              context: context,
-              builder: (BuildContext context) => SubCategoryFormDialog(
-                subCategoryToUpdate: subCategory,
-                category: widget.category,
-              ),
-            );
-          },
-        ),
-        IconButton(
-          icon: Icon(Icons.delete_outline),
-          onPressed: () {
-            _subCategoryBloc.add(DeleteSubCategoryEvent(subCategory));
-          },
-        ),
-      ],
-    );
-  }*/
 }
